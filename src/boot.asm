bf15ab2702242c864b16cc954b6213ce00474c57
Mohammed abdelkafi <kawruulife@gmail.com>
2025-03-14 22:24:06 +0100

    Update boot.asm

--- src/boot.asm
+++ src/boot.asm
@@ -1,23 +1,37 @@
 org 0x7C00
+bits 16
+main:
+    mov ah, 0x0e
+    mov al, 'B'
+    int 0x10
+    mov al, 'o'
+    int 0x10
+    mov al, 'o'
+    int 0x10
+    mov al, 't'
+    int 0x10
+    mov al, 'i'
+    int 0x10
+    mov al, 'n'
+    int 0x10
+    mov al, 'g'
+    int 0x10
+    mov al, ' '
+    int 0x10
+    mov al, '.'
+    int 0x10
+    mov al, ' '
+    int 0x10
+    mov al, '.'
+    int 0x10
+    mov al, ' '
+    int 0x10
+    mov al, '.'
+    int 0x10
 
-section .text
-    mov si, msg
-
-print_loop:
-    lodsb
-    or al, al
-    jz done
-
-    mov ah, 0x0E 
-    mov bh, 0x00 
-    int 0x10 
-
-    jmp print_loop
-
-done:
+    cli
     hlt
-
-msg db "Welcome To Lithium!", 0 
+    jmp $
 
 times 510-($-$$) db 0
 dw 0xAA55
